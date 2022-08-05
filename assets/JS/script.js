f0f5a16c6daaf10e09e36fe305a4a540b4f4afeb
Octi Muri <47183242+Octiii@users.noreply.github.com>
2022-08-05 15:11:38 +0000

    Added check to see if cell is empty and prevent same cell to be played multiple times.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,6 +5,8 @@ let boardData = [
   [0, 0, 0,]
 ]
 
+//Determines whos turn it is 
+let player = 1;
 
 //Pull cell class divs from DOM.
 const cellDiv = document.querySelectorAll(".cell");
@@ -20,6 +22,11 @@ function placeMarker(index) {
   //Determine index position
   let col = index % 3
   let row = (index - col) / 3
-  boardData[row][col] = 1;
-  console.log(boardData);
+  //Check if current cell is empty 
+  if (boardData[row][col] == 0) {
+    boardData[row][col] = player;
+    //Change player
+    player *= -1;
+    console.log(boardData);
+  }
 }
