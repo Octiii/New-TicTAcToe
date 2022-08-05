1487009b3336bdbd0b82bd34bf3316114fa39f76
Octi Muri <47183242+Octiii@users.noreply.github.com>
2022-08-05 13:21:36 +0000

    Cliking on a cell div now gives an index of wich cell has been cliked.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,11 @@
 
+
+
 //Pull cell class divs from DOM.
 const cellDiv = document.querySelectorAll(".cell");
 //Event listner for cells.
-cellDiv.forEach(cell => {
+cellDiv.forEach((cell, index) => {
   cell.addEventListener("click", () => {
-    console.log("ping")
-  })
-})
+    console.log(index)
+  });
+});
